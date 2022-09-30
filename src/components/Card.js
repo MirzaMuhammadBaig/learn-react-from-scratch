917bb4f420762b35366227fc0c7b942e48269920
MirzaMuhammadBaig <mbaig4893@gmail.com>
2022-09-30 19:36:31 +0500

    MakeAnotherCardInCardComponent

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import MyImage from '../img/logo192.png'
 
 function Card() {
   return (
     <>
-        <br></br>
+        {/* <br></br>
         <h1>My awesome chart in react</h1>
         <br></br>
         <h3>Reason I love react</h3>
@@ -12,6 +13,15 @@ function Card() {
             <li>It's declarative</li>
             <li>It's a hireable skill</li>
             <li>It's actively maintained by skilled people</li>
+        </ol> */}
+        <img src={MyImage} alt='Image' style={{width: "100px"}} />
+        <h1>Fun Facts About React</h1>
+        <ol>
+            <h5><li>Was first released in 2013</li></h5>
+            <h5><li>Was originally created by Jorden Walke</li></h5>
+            <h5><li>Has well over 100k stars on Github</li></h5>
+            <h5><li>Is maintained by facebook</li></h5>
+            <h5><li>Power thousand of enterprise apps, including mobile apps</li></h5>
         </ol>
     </>
   )
