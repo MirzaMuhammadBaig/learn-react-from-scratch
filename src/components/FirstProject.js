3d936f0593ec2b50cac917d0d16322ce478cae48
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2022-10-02 09:20:21 +0500

    FirstProjectComplete

--- src/components/FirstProject.js
+++ src/components/FirstProject.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Image from '../img/logo192.png'
+import '../FirstProject.css'
 
 function FirstProject() {
   return (
-    <div>
-        
-    </div>
+    <>
+        <navbar className='navbar1'>
+            <img src={Image} alt="logo" />
+            <h1>ReactFacts</h1>
+            <h3>React Course-Project 1</h3>
+        </navbar>
+        <section>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jorden Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </section>
+    </>
   )
 }
 
