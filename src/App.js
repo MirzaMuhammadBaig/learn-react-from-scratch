b401036778852378d44aceddf05c791baa142f4f
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2022-10-02 08:48:37 +0500

    CreateComponentOfFirstProject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import FirstProject from "./components/FirstProject";
 
 function App() {
   return (
     <>
+    <FirstProject/>
     </>
   );
 }
---
+++ src/components/FirstProject.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstProject() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default FirstProject
