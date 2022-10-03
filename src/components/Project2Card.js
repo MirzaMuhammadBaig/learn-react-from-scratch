0239949842c11af164c8bd498ca314eea7694f97
MirzaMuhammadBaig <muhammadbaig090@gmail.com>
2022-10-03 08:49:36 +0500

    Complete●map

--- src/components/Project2Card.js
+++ src/components/Project2Card.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import Project2Body from './Project2Body'
+// import Project2Body from './Project2Body'
+// import Project2Data from './Project2Data'
 
-function Project2Card() {
+function Project2Card(props) {
 
-
+  let badgeText
+  if(props.openSpots === 0){
+    badgeText = 'Sold Out'
+  }
+  else if(props.badgeText !== 0){
+    badgeText= 'Online'
+  }
 
   return (
     <>
-        <Project2Body/>
+        <div>
+          <div>{badgeText}</div>
+          <img src={props.item.img} alt="" />
+          <div>
+            <span>★ {props.item.stats.rating}</span>
+            <span>{props.item.stats.reviewCount} </span>
+            <span> ●{props.item.location}</span>
+          </div>
+          <div>
+            <p>{props.item.description}</p>
+            <p>From ${props.item.price} / Person</p>
+          </div> 
+        </div>
     </>
   )
 }
--- src/components/Project2Data.js
+++ src/components/Project2Data.js
@@ -1,69 +1,71 @@
-
+import React from 'react'
 import pic1 from '../img/pic1.jpg'
 import pic2 from '../img/pic2.jpg'
 import pic3 from '../img/pic3.jpg'
 import pic4 from '../img/pic4.jpg'
 
-export default () =>  {
+function Project2Data() {
 
-    Data = [
+    let data = [
         {
             id:1,
             img : pic1,
             title: "First title.",
-            description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae nobis perspiciatis fuga commodi illo! Placeat, consequuntur accusantium consectetur maxime rerum asperiores sequi!',
+            description : 'Lorem ipsum dolor sit, perspiciatis fuga commodi illo!',
             price: 136,
             coverimg: "Nature-Pic-1.jpg",
             stats: {
                 rating: 5.0,
                 reviewCount: 6
             },
-            location: "Online",
+            location:"Pakistan",
             openSpots: 1,
         },
         {
             id:2,
             img : pic2,
             title: "Second title.",
-            description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae nobis perspiciatis fuga commodi illo! Placeat, consequuntur accusantium consectetur maxime rerum asperiores sequi!',
+            description : ' Molestiae nobis perspiciatis fuga commodi illo! Placeat.',
             price: 125,
             coverimg: "Nature-Pic-2.jpg",
             stats: {
                 rating: 4.9,
                 reviewCount: 5
             },
-            location: "Soldout",
+            location: "USA",
             openSpots: 0,
         },
         {
             id:3,
             img : pic3,
             title: "Third title.",
-            description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae nobis perspiciatis fuga commodi illo! Placeat, consequuntur accusantium consectetur maxime rerum asperiores sequi!',
+            description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
             price: 100,
             coverimg: "Nature-Pic-3.jpg",
             stats: {
                 rating: 4.8,
                 reviewCount: 3
             },
-            location: "Online",
-            openSpots: 0,
+            location:"Pakistan",
+            openSpots: 1,
         },
         {
             id:4,
             img : pic4,
             title: "Fourth title.",
-            description : 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae nobis perspiciatis fuga commodi illo! Placeat, consequuntur accusantium consectetur maxime rerum asperiores sequi!',
+            description : 'Consequuntur accusantium consectetur maxime rerum asperiores sequi!',
             price: 75,
             coverimg: "Nature-Pic-4.jpg",
             stats: {
                 rating: 4.7,
                 reviewCount: 3
             },
-            location: "SoldOut",
+            location: "USA",
             openSpots: 0,
         }
     ]
+
+    console.log(data)
 
   return (
     <>
@@ -72,3 +74,4 @@ export default () =>  {
   )
 }
 
+export default Project2Data
